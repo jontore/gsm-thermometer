49a61b4006a21c016b302330763bdde19dfd003b
Jon Tore Hafstad <jontore@gmail.com>
2015-04-10 22:08:50 +0200

    Fixed proper string concat

--- gsm-termo/gsm-termo.ino
+++ gsm-termo/gsm-termo.ino
@@ -33,17 +33,16 @@ void setup()
   lcd.begin(20, 4);
   lcd.home();
   printToScreen("init");
+  delay(2000);
 }
 
 void loop() {
-  lcd.display();
-  measureAndPost();
-  lcd.clear();
-  lcd.noDisplay();
+  measureAndPost(); 
   delay(SLEEP_TIME);
 }
 
 char msg[1];
+char temp[150];
 int measureAndPost () {
   boolean started = false;
   printToScreen("Connecting");
@@ -61,9 +60,9 @@ int measureAndPost () {
       return 0;
     }
 
+    printToScreen("Reading sensors");
     delay(1000);
-
-    char temp[50];
+  
     readSensors(temp);
     printToScreen("Updating server");
     delay(2000);
@@ -79,6 +78,7 @@ int measureAndPost () {
 
 void printToScreen(String str) {
   lcd.home();
+  lcd.setCursor(0, 0);
   lcd.clear();
   lcd.print(str);
   printTemp();
@@ -90,27 +90,32 @@ void printTemp() {
   lcd.print(int(dht.getTemperature()));
   lcd.print(" h: ");
   lcd.print(int(dht.getHumidity()));
-  int airQuality = analogRead(AIRQUALITY_SENSOR);
   lcd.print(" aq: ");
-  lcd.print(airQuality);
+  lcd.print(analogRead(AIRQUALITY_SENSOR));
 }
+
+char *preTmp = "t%5Btemperature%5D=";
+char *preHum = "\&t%5Bhumidity%5D=";
+char *preAirQuality = "\&t%5Bair_quality%5D=";
 
 void readSensors(char* input) {
   float temp = dht.getTemperature();
-  float humidity = dht.getHumidity();
-  int airQuality = analogRead(AIRQUALITY_SENSOR);
+  float hum = dht.getHumidity();
+  float aq = (float)analogRead(AIRQUALITY_SENSOR);
 
-  char tmp[20];
-  char *preTmp = "temperature%5Btemperature%5D=";
-  char *preHum = "&temperature%5Bhumidity%5D=";
-  char *preAirQuality = "&temperature%5Bair_quality%5D=";
-  dtostrf(temp, 4, 3, tmp);
-  strcpy(input, preTmp);
-  strcat(input, tmp);
-  dtostrf(humidity, 4, 3, tmp);
-  strcat(input, preHum);
-  strcat(input, tmp);
-  dtostrf(airQuality, 4, 3, tmp);
-  strcat(input, preAirQuality);
-  strcat(input, tmp);
+  strcpy(input, "");
+  copyAndCat(input, preTmp, temp);
+  copyAndCat(input, preHum, hum);
+  copyAndCat(input, preAirQuality, aq);
 };
+
+void copyAndCat(char* input, char* argument, float measurement) {
+  char tmp[20];
+  char tempStr[50];
+  dtostrf(measurement, 4, 3, tmp);
+  strcpy(tempStr, argument);
+  strcat(tempStr, tmp);
+  strcat(input, tempStr);
+}
+
+
